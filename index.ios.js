2f5bc7e04165df0345d03abf1604f31d57aa5a3b
Jeffrey Farber <jeff.farber@gmail.com>
2017-02-08 17:23:56 -0800

    initial commit of code from class

---
+++ index.ios.js
@@ -0,0 +1,83 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ListView,
+  Button
+} from 'react-native';
+
+export default class NuevaTodo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todo: '',
+      todos: ['row 1', 'row 2', 'row 3', 'todo 1', 'todo 2'],
+    };
+  }
+
+  render() {
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    var dataSource = ds.cloneWithRows(this.state.todos);
+
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.todo}
+          placeholder="New Todo"
+          onChangeText={(text) => this.setState({todo: text})}
+        />
+        <Button
+          style={styles.searchButton}
+          onPress={() => this.setState({todos: this.state.todos.concat(this.state.todo)})}
+          title="Go!"
+        />
+
+        <ListView
+          dataSource={dataSource}
+          renderRow={(rowData) => <Text>{rowData}</Text>}
+        />
+
+        <Text style={styles.instructions}>
+          {this.state.todo}
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  todo: {
+    height: 40,
+  }
+});
+
+AppRegistry.registerComponent('NuevaTodo', () => NuevaTodo);
