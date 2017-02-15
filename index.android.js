8e7a75d28750054ea8419e543339217fb4b18764
Jen Selby <jselby@nuevaschool.org>
2017-02-14 22:21:23 -0800

    Add changes for Android as well.

--- index.android.js
+++ index.android.js
@@ -9,18 +9,40 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput,
+  ListView,
+  Button
 } from 'react-native';
+import TodoList from './todoList.js';
 
 export default class NuevaTodo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todo: '',
+      todos: ['row 1', 'row 2', 'row 3', 'todo 1', 'todo 2'],
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
+        <TextInput
+          style={styles.todo}
+          placeholder="New Todo"
+          onChangeText={(text) => this.setState({todo: text})}
+        />
+        <Button
+          style={styles.searchButton}
+          onPress={() => this.setState({todos: this.state.todos.concat(this.state.todo)})}
+          title="Go!"
+        />
+
+        <TodoList todos={this.state.todos} />
+
         <Text style={styles.instructions}>
-          To get started, edit index.android.js
+          {this.state.todo}
         </Text>
         <Text style={styles.instructions}>
           Double tap R on your keyboard to reload,{'\n'}
@@ -48,6 +70,9 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  todo: {
+    height: 40,
+  }
 });
 
 AppRegistry.registerComponent('NuevaTodo', () => NuevaTodo);
